4d5615ff131e6fa8fb5ecaebbf8e3e0004ae8572
Henri Lopes <82175620+henrilopes1@users.noreply.github.com>
2025-03-24 21:25:12 -0300

    Delete fiap-soa-checkpoint-1 directory

--- fiap-soa-checkpoint-1/src/main/java/com/example/api/exception/ValidacaoPedidoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.api.exception;
-
-public class ValidacaoPedidoException extends RuntimeException {
-    public ValidacaoPedidoException(String mensagem) {
-        super(mensagem);
-    }
-} 
--- fiap-soa-checkpoint-1/src/main/java/com/example/api/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.api.repository;
-
-import com.example.api.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
